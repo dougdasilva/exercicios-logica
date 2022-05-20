ee2e2f6ed50a19dd094c4e1b40926b1ff1db61ca
dougdasilva <douglas_wfs@hotmail.com>
2022-05-20 19:14:14 -0300

    exercicios de logica de programacao em csharp

---
+++ Exercicio1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstudosLógica
+{
+    internal class Exercicio1
+    {
+        static void Main(string[] args)
+        {
+
+            Random random = new Random();
+
+            int choice = 0;
+            int attempts = 0;
+            int rand = random.Next(11);
+            Console.WriteLine("-----> " + rand + " <-----");
+
+            do
+            {
+                Console.WriteLine("Choose a number: ");
+                choice = int.Parse(Console.ReadLine());
+                if (rand < choice)
+                {
+                    Console.WriteLine("Error! The number is less than: " + choice);
+                }
+                else if(rand > choice)
+                {
+                    Console.WriteLine("Error! The number is greather than: " + choice);
+                }
+                attempts++;
+            } while (choice != rand);
+                Console.WriteLine("Right in the bull's-eye! The number is: " + rand);
+                Console.WriteLine("Attemps: " + attempts);
+
+            Console.ReadLine();
+        }
+    }
+}
